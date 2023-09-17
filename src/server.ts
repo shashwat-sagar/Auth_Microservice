3a732b118bc2b1a5a1446b61688c60c8bfef66ac
Shashwat Sagar <59369746+badass1901@users.noreply.github.com>
2023-09-17 17:24:36 +0530

    test4 lint staged

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 function testThis(username: string): string {
-    console.log(username);
-    const user = { nae: "test",
-                                                                                                                                                                                                                age: 12,
-    };
-    const name = user["nae"];
+    // console.log(username);
+    const user = { nae: "test", age: 12 };
+    const name = user.nae;
 
     return username + name;
 }
