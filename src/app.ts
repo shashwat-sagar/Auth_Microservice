36743199f28c2b7b0a615db30c1200724c5b1cb1
Shashwat Sagar <59369746+badass1901@users.noreply.github.com>
2023-09-19 02:12:00 +0530

    test commit for eslint

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Auth Service");
+    res.send("Welcome to Auth Services");
 });
 
 export default app;
