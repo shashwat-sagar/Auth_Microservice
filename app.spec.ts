36f3a2dafd25c6e346343092e5abaf89bc6919d8
badass1901 <shashwatsagar19@gmail.com>
2023-10-02 00:23:14 +0530

    added test setup using jest

---
+++ app.spec.ts
@@ -0,0 +1,15 @@
+import app from "./src/app";
+import { calculateDiscount } from "./src/utils";
+import request from "supertest";
+
+describe("App", () => {
+    it("should calculate the discount", () => {
+        const result = calculateDiscount(100, 15);
+        expect(result).toBe(85);
+    });
+
+    it("should return 200 status code", async () => {
+        const response = await request(app).get("/").send();
+        expect(response.statusCode).toBe(200);
+    });
+});
